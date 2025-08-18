d2a0c529f0ef78f57b2221f12f0b340f8c3b9e9c
Eric Sorum <ejsorum@gmail.com>
2025-08-18 08:17:55 -0500

    Added vercel blob to img urls

--- next.config.js
+++ next.config.js
@@ -10,14 +10,17 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      // existing pattern for your site URL
+      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
         const url = new URL(item)
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
+        return { hostname: url.hostname, protocol: url.protocol.replace(':', '') }
       }),
+      // allow Vercel Blob public URLs
+      {
+        protocol: 'https',
+        hostname: '**.public.blob.vercel-storage.com',
+        pathname: '/**',
+      },
     ],
   },
   webpack: (webpackConfig) => {
